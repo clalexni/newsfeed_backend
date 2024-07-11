9ba596707b0fde90f0a5b5474012833bd8ab8d7c
clalexni <cl.alexni@gmail.com>
2024-07-10 22:40:10 -0500

    init django rest framework + basic accounts API

---
+++ accounts/api/views.py
@@ -0,0 +1,12 @@
+from rest_framework import viewsets, permissions
+from django.contrib.auth.models import User
+from accounts.api.serializers import UserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
